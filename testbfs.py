127b20bae224737edb199311d8ab0879d2668ccc
IAmMarcelJung <MarcelJung97@web.de>
2023-12-14 17:41:12 +0100

    refactor: remove unneeded lines

--- testbfs.py
+++ testbfs.py
@@ -10,21 +10,13 @@ from fabric_generator.file_parser import parseMatrix
 from search_path.utils import transpose_csv, create_data
 from search_path.graph import *
 
-generic_dir = "./fabric_files/generic/"
-csvfile = "./tb_test/Tile/LUT4AB/" + "LUT4AB_switch_matrix.csv"
-csvfile_transposed = "./tb_test/Tile/LUT4AB/" + "LUT4AB_switch_matrix_transposed.csv"
 test_file = "test_features.txt"
 
 class TestBfs(unittest.TestCase):
-    data = parseMatrix(csvfile, "LUT4AB")
-    data_transposed = parseMatrix(csvfile_transposed, "LUT4AB")
-    sp = SearchPath(data, data_transposed)
     best = sp.Path(list(), sys.maxsize)
     current_path = sp.Path(list(), 0)
 
     def setUp(self):
-        self.data_transposed = parseMatrix(csvfile, "LUT4AB")
-        transpose_csv(csvfile, csvfile_transposed)
         self.best = self.sp.Path(list(), sys.maxsize)
         self.current_path = self.sp.Path(list(), 0)
     def tearDown(self):
