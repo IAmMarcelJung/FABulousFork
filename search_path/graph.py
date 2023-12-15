1e941be8cffc4d50f832d0399a47c90400f27c49
IAmMarcelJung <MarcelJung97@web.de>
2023-12-15 17:23:54 +0100

    feat: also find external pips for a tile

--- search_path/graph.py
+++ search_path/graph.py
@@ -2,33 +2,34 @@
 from collections import deque
 from typing import *
 #import pdb; pdb.set_trace()
+from dataclasses import dataclass
 
-class Node:
-    def __init__(self, internal_parents: Set, internal_children: Set, external_parents: Set, external_children: Set):
-        self.internal_parents = internal_parents
-        self.internal_children = internal_children
-        self.external_parents = external_parents
-        self.external_children = external_children
-        self.tile = Tile(0,0)
-        self.name = ""
-
+@dataclass(frozen=True)
 class Tile:
-    def __init__(self, x: int, y: int):
+    '''def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
+        '''
+    x: int
+    y: int
 
-def create_graph_from_file(pip_file):
-    graph = PipeGraph()
-    with open(pip_file) as f:
-        for line in f:
-            if line.startswith('#'):
-                continue
-            # Split the line into its components
-            source_tile, sink, destination_tile, source, dnc, feature = line.split(',')
-            # Create a pipe node and add it to the graph
-            pipe_node = PipeNode(source_tile, source, destination_tile, sink, _)
-            graph.add_node(pipe_node)
-    return graph
+@dataclass(frozen=True)
+class Node_Header:
+    '''def __init__(self, name: str, tile: Tile):
+        self.tile = tile
+        self.name = name
+        '''
+    name: str
+    tile: Tile
+
+class Node:
+    def __init__(self, header: Node_Header):
+        self.internal_parents = set()
+        self.internal_children = set()
+        self.external_parents = set()
+        self.external_children = set()
+        self.tile = header.tile
+        self.name = header.name
 
 def tile_to_str(tile: Tile):
     return f"X{tile.x}Y{tile.y}"
@@ -55,13 +56,17 @@ def get_nodes_from_file_for_tile(pip_file: str, tile: Tile):
             if is_current_tile or (tile_str in line and line.startswith("#")):
                 is_current_tile = True
                 if not line.startswith("#"):
-                    source_tile, sink, destination_tile, source, dnc, feature = line.split(',')
-                    nodes.add(sink)
-                    nodes.add(source)
+                    source_tile, source, sink_tile, sink, dnc, feature = line.split(',')
+                    source_tile = str_to_tile(source_tile)
+                    sink_tile = str_to_tile(sink_tile)
+                    source_node = Node_Header(source, source_tile)
+                    sink_node = Node_Header(sink, sink_tile)
+                    nodes.add(source_node)
+                    nodes.add(sink_node)
     return nodes
 
 def add_parents_and_children(pip_file: str, tile: Tile, nodes_set: Set):
-    nodes = { key: Node(set(), set(), set(), set()) for key in nodes_set}
+    nodes = {key: Node(key) for key in nodes_set}
     tile_str = tile_to_str(tile)
     next_tile = Tile(tile.x + 1, tile.y)
     next_tile_str = tile_to_str(next_tile)
@@ -75,23 +80,28 @@ def add_parents_and_children(pip_file: str, tile: Tile, nodes_set: Set):
             if is_current_tile or (tile_str in line and line.startswith("#")):
                 is_current_tile = True
                 if not line.startswith("#"):
-                    source_tile, source, destination_tile, sink, dnc, feature = line.split(',')
-                    if source_tile == destination_tile:
-                        nodes[sink].internal_parents.add(source)
-                        nodes[source].internal_children.add(sink)
+                    source_tile, source, sink_tile, sink, dnc, feature = line.split(',')
+                    source_tile = str_to_tile(source_tile)
+                    sink_tile = str_to_tile(sink_tile)
+                    source_node = Node_Header(source, source_tile)
+                    sink_node = Node_Header(sink, sink_tile)
+                    if source_tile == sink_tile:
+                        nodes[sink_node].internal_parents.add(source_node)
+                        nodes[source_node].internal_children.add(sink_node)
                     else:
-                        nodes[sink].external_parents.add(source)
-                        nodes[source].external_children.add(sink)
-                    nodes[sink].name = sink
-                    nodes[sink].tile = destination_tile
-                    nodes[source].name = source
+                        nodes[sink_node].external_parents.add(source_node)
+                        nodes[source_node].external_children.add(sink_node)
+                    nodes[sink_node].name = sink
+                    nodes[sink_node].tile = sink_tile
+                    nodes[source_node].name = source
+                    nodes[source_node].tile = source_tile
     return nodes
 
-def append_paths(paths: List, current_node: str, graph: Dict, visited: Set):
+def append_paths(paths: List, node: Node_Header, graph: Dict, visited: Set):
     new_paths = []
     for path in paths:
-        if current_node in path:
-            for child in graph[current_node].internal_children:
+        if node in path:
+            for child in {*graph[node].internal_children, *graph[node].external_children}:
                 if child not in visited and child not in path:
                     new_list = path.copy()
                     new_list.append(child)
@@ -106,32 +116,32 @@ def get_lists_where_last_element_matches(lists: List, elem: str):
     return result_lists
 
 
-def bfs(graph: Dict, start_node_name: str, end_node_name: str):
+def bfs(graph: Dict, start_node: Node_Header, end_node: Node_Header):
     queue = deque()
     visited = set()
-    current_node = start_node_name
+    current_node = start_node
     visited.add(current_node)
-    paths = [[start_node_name]]
+    paths = [[start_node]]
     append_paths(paths, current_node, graph, visited)
 
-    while current_node != end_node_name:
-        if graph[current_node].internal_children:
-            for child in graph[current_node].internal_children:
-                if child not in visited:
-                    queue.append(child)
+    while current_node != end_node:
+        for child in {*graph[current_node].internal_children, *graph[current_node].external_children}:
+            if child not in visited:
+                queue.append(child)
         if not queue:
             break
         current_node = queue.popleft()
         append_paths(paths, current_node, graph, visited)
         visited.add(current_node)
 
-    return get_lists_where_last_element_matches(paths, end_node_name)
+    return get_lists_where_last_element_matches(paths, end_node)
 
 def create_features(path: List):
     feature_list = []
     for i in range(len(path[:-1])):
         feature = f"{path[i]}.{path[i+1]}"
         feature_list.append(feature)
+
     return feature_list
 
 def append_features_to_file(features: List, file: str):
