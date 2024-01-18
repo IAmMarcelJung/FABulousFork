a2ae3a032354df0d6fc82a5402a87660c9da4174
IAmMarcelJung <MarcelJung97@web.de>
2024-01-18 16:17:39 +0100

    fix: use single thread instead of multithreading, since the overhead makes the code slow

--- create_inverters.py
+++ create_inverters.py
@@ -2,6 +2,7 @@
 import multiprocessing
 import argparse
 import os
+import copy
 
 from joblib import Parallel, delayed
 from tqdm import tqdm
@@ -33,9 +34,8 @@ def search_in_tile(graph: Dict, tile: Tile, start: str, end: str, mapping: Mappi
 def parse_arguments():
     parser = argparse.ArgumentParser(
             prog='create_inverters',
-            description='Create inverters in a FABulous fabric.',
-            epilog='Text at the bottom of help')
-    parser.add_argument("directory", help="Path to the project directory")
+            description='Create inverters in a FABulous fabric.')
+    parser.add_argument("-d", "--directory", help="Path to the project directory.")
 
     args = parser.parse_args()
 
@@ -48,32 +48,33 @@ def parse_arguments():
 if __name__ == "__main__":
 
     cpu_cores = min(multiprocessing.cpu_count(), 32)
-    print(f"Using {cpu_cores} cores")
+
     args = parse_arguments()
     project_directory = args.directory
+
     luts_in_tile = ["A", "B", "C", "D", "E", "F", "G", "H"]
     pip_file = f"{project_directory}/.FABulous/pips.txt"
     fabric_file = f"{project_directory}/fabric.csv"
+
     tile_type = Tile.Types.LUT4AB
     #tile_type = Tile.Types.RAM_IO
+
     mapping = Mapping()
 
     print("Creating graph...")
     graph = create_graph(pip_file, mapping)
-    for key in graph.keys():
-        if graph.uid_to_node_header[key] == "A_O":
-            print("FOUND A_O")
 
     tiles = get_tiles_for_fabric(fabric_file)
     tiles = get_all_locations_of_tile_type(tile_type, tiles)
-    tiles = tiles[:1]
+    #tiles = tiles[:5]
 
     paths = []
     print("Searching for possible paths:")
     for lut in tqdm(luts_in_tile):
         start = f"L{lut}_O"
         end = f"L{lut}_I3"
-        tmp_paths = list(
+        """
+        paths += list(
                 tqdm(
                     Parallel(return_as="generator", n_jobs=cpu_cores)(
                         delayed(search_in_tile)(graph, tile, start, end, mapping) for tile in tiles
@@ -81,17 +82,16 @@ if __name__ == "__main__":
                     total=len(tiles),
                 )
             )
-        for tmp_path in tmp_paths:
-            paths.append(tmp_path)
+        """
+        for tile in tqdm(tiles):
+            paths.append(search_in_tile(graph, tile, start, end, mapping))
 
-    print(paths)
     header_node_paths = convert_and_sort(paths, mapping)
-    print(header_node_paths)
     features = []
     used_tiles = set()
+
     for path in header_node_paths:
         features += create_features(path, used_tiles)
-
 
     try:
         append_features_to_file(features, f"{project_directory}/user_design/sequential_16bit_en.fasm")
